5bb3fa734ac5d41b8af729a5c911ab3b67250952
LauraT09 <lautov0911@gmail.com>
2025-06-17 11:55:56 -0500

    All changes

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProcessingOrderAPI.Models;
+using ProcessingOrderAPI.Services;
+
+namespace ProcessingOrderAPI.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : Controller
+    {
+        private readonly ICSVService _csvService;
+
+        public OrderController(ICSVService csvService)
+        {
+            _csvService = csvService;
+        }
+        //C:\\Users\\Laura Torres\\Documents\\Mis docus\\NielsenIQ\\Problem\\values.csv
+        [HttpPost("read-orders-csv")]
+        public async Task<IActionResult> GetOrdersCSV([FromForm] IFormFileCollection file)
+        {
+            var orders = _csvService.ReadCSV<Orders>(file[0].OpenReadStream());
+
+            return Ok(orders);
+        }
+    }
+}
+
---
+++ Models/Orders.cs
@@ -0,0 +1,14 @@
+﻿namespace ProcessingOrderAPI.Models
+{
+    public class Orders
+    {
+        //type, orderno, description, value, qty
+        public string Type { get; set; }
+        public decimal OrderNumber { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Value { get; set; }
+        public decimal Quantity { get; set; }
+    }
+}
